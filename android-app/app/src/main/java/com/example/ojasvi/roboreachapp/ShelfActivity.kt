7f39b656ec6be28bfa78e66c77288d2bb55039d9
Ojasvi-Jalal <s1612970@sms.ed.ac.uk>
2019-02-01 16:41:48 +0000

    edit buttons on the card

--- android-app/app/src/main/java/com/example/ojasvi/roboreachapp/ShelfActivity.kt
+++ android-app/app/src/main/java/com/example/ojasvi/roboreachapp/ShelfActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.support.design.widget.FloatingActionButton
 
 class ShelfActivity : AppCompatActivity() {
 
@@ -14,10 +16,17 @@ class ShelfActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         // Edit button trigger
-        val editButton = findViewById<Button>(R.id.De)
-        editButton.setOnClickListener {
-            startActivity(Intent(this, EditActivity::class.java))
+        val editButton1 = findViewById<FloatingActionButton>(R.id.editButton1)
+        val editButton2 = findViewById<FloatingActionButton>(R.id.editButton2)
+
+        editButton1.setOnClickListener {
+            val mEdit = findViewById<EditText>(R.id.expiry)
+            mEdit.isEnabled= true
         }
 
+        editButton2.setOnClickListener {
+            val mEdit = findViewById<EditText>(R.id.barcode)
+            mEdit.isEnabled = true
+        }
     }
 }
