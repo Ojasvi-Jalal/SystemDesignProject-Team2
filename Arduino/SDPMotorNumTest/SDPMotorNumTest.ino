25b6f443632069ad78a5b301d72f38f6cbaa764f
aida <33495958+aidaFdez@users.noreply.github.com>
2019-02-05 12:53:07 +0000

    Added file to try the sensor

--- Arduino/SDPMotorNumTest/SDPMotorNumTest.ino
+++ Arduino/SDPMotorNumTest/SDPMotorNumTest.ino
@@ -8,35 +8,6 @@ void setup(){
 }
 
 void loop(){
-Serial.println("Motor 0 50%");
-motorForward(0, 50);
-delay(2500);
-motorAllStop();
-
-Serial.println("Motor 1 50%");
-motorForward(1, 50);
-delay(2500);
-motorAllStop();
-
-Serial.println("Motor 2 50%");
-motorForward(2, 50);
-delay(2500);
-motorAllStop();
-
-Serial.println("Motor 3 50%");
-motorForward(3, 50);
-delay(2500);
-motorAllStop();
-
-Serial.println("Motor 4 50%");
-motorForward(4, 50);
-delay(2500);
-motorAllStop();
-
-Serial.println("Motor 5 50%");
-motorForward(5, 50);
-delay(2500);
-motorAllStop();
-
+    int a = digitalRead(A3);
+    Serial.println(a);
 }
-
