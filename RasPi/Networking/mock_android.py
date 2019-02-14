a1ce31effc7920674ac46af87fc74809390c8a08
Ben Banerjee-Richards <dev@benbr.com>
2019-02-14 13:53:30 +0000

    Added mock database interactions and socketio endpoints for these

--- RasPi/Networking/mock_android.py
+++ RasPi/Networking/mock_android.py
@@ -11,6 +11,18 @@ def on_connect():
 
 @sio.on('move_to')
 def move_to_response(res):
+    print("move_to")
+    print(res)
+
+@sio.on("get_data")
+def get_data(res):
+    print("get_data")
+    print(res)
+
+
+@sio.on("add_item")
+def add_item(res):
+    print("add_item")
     print(res)
 
 def main():
@@ -19,6 +31,16 @@ def main():
     sio.emit("move_to", {"pos": 10})
     sio.emit("move_to", {"pos": -3})
     sio.emit("move_to", {"pos": 5})
+
+    sio.emit("get_data")
+    sio.emit("add_item", {"name": "No Pos!"})
+    sio.emit("add_item", {"pos": 12, "name": "Pineapple", "barcode": "394830242"})
+    sio.emit("get_data")
+    sio.emit("remove_item",{})
+    sio.emit("remove_item",{"pos": 2})
+    sio.emit("remove_item",{"pos": 20})
+    sio.emit("get_data")
+
     sio.wait()
 
 if __name__ == "__main__":
--- RasPi/Networking/serve.py
+++ RasPi/Networking/serve.py
@@ -8,9 +8,44 @@ from flask_socketio import SocketIO, send, emit
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-    
+MOCK_DATA = [
+    {"name": "Jam",
+    "pos": 2,
+    "expiry": "2019-10-03"
+    },
+    {"name": "Grape",
+    "pos": 1,
+    }
+]
+
+# Mock interactions with the database
+# These will be replaced with actual database stuff once that's been completed
+def db_get_all():
+    return MOCK_DATA
+
+def db_add(pos, name, expiry, barcode):
+    MOCK_DATA.append({
+        "name": name,
+        "pos": pos,
+        "expiry": expiry,
+        "barcode": barcode
+    })
+
+def db_remove(pos: int):
+    to_remove = None
+    for idx, item in enumerate(MOCK_DATA):
+        if item.get("pos") == pos:
+            to_remove = idx
+            break
+
+    if to_remove is None:
+        return False
+
+    del MOCK_DATA[idx]
+    return True
+
 # Move the shelf to a position. Example
-# move_to, {pos: "8"}
+# move_to, {"pos": 8}
 # This endpoint probabily won't be needed in the future but useful for testing
 @socketio.on('move_to')
 def handle_message(message):
@@ -25,6 +60,32 @@ def handle_message(message):
 
     sio.write_char(pos.__str__())
     emit("move_to", {"success": True})
+
+@socketio.on("get_data")
+def get_data():
+    emit("get_data", MOCK_DATA)
+
+@socketio.on("add_item")
+def add_item(item):
+    if "pos" not in item:
+        emit("add_item", {"success": False, "message": "No pos on item"})
+        return 
+
+    db_add(item.get("pos"), item.get("name"), item.get("expiry"), item.get("barcode"))
+    emit("add_item", {"success": True})
+
+@socketio.on("remove_item")
+def remove_item(json):
+    if "pos" not in json:
+        emit("add_item", {"success": False, "message": "No pos provided"})
+        return 
+
+    if not db_remove(json["pos"]):
+        emit("add_item", {"success": False, "message": "Could not find an item with that pos to remove"})
+        return 
+
+    emit("remove_item", {"success": True})
+
 
 if __name__ == '__main__':
     global sio
