a17fe1b2cddea6e16f49cc7301908c25a3db3555
Andrew <21962412+ndrewfrost@users.noreply.github.com>
2019-02-22 18:24:24 +0000

    Move Folder

--- RasPi/Networking/json/Segment.py
+++
@@ -1,9 +0,0 @@
-import json
-
-class Segment:
-	
-	def __init__(self, shelfID, itemName, expiryDate, barcode):
-		self.shelfID = shelfID
-		self.itemName = itemName
-		self.expiryDate = expiryDate
-		self.barcode = barcode
---
+++ RasPi/json/Item.py
@@ -0,0 +1,6 @@
+
+class Item:
+
+	def __init__(self, barcode, name):
+		self.name = name
+		self.barcode =  barcode
