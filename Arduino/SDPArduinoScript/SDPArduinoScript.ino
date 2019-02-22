9220220b1cb5c87b709efd904daa585841050370
Sinan Vanlı <sinan.vanli@gmail.com>
2019-02-22 16:56:14 +0000

    Moved the shelf coordinate computations to different file.
    instead of using memory focused computation, we now do mathematical 
    computation. Shelf dimensions can and should be changed on Shelf.cpp 
    if/when the physical shelf is changed.

--- Arduino/SDPArduinoScript/SDPArduinoScript.ino
+++ Arduino/SDPArduinoScript/SDPArduinoScript.ino
@@ -1,47 +1,36 @@
 #include "SDPArduino.h"
 #include <Wire.h>
+#include "Shelf.h"
 int state = 0;
 int height = 0;
 int pos [2] = {0,0};
-int shelf [9][2] = {{0,0}, {1,0}, {2, 0}, {3, 0}, {4, 0},
-                           {1,1}, {2, 1}, {3, 1}, {4, 1}};
-
 //Position of the robot. The first entry of the array corresponds to x, the second to y
 int position[2] = {0,0};
 
 void setup(){
-  SDPsetup();
-  while(!Serial){;} // Wait until serial is established
-  Serial.println("Serial Connected");
-  Serial.println("Going to Origin");
-  goOrigin();
+    SDPsetup();
+    while(!Serial){;} // Wait until serial is established
+    Serial.println("Serial Connected");
+    Serial.println("Going to Origin");
+    goOrigin();
 }
 
-/*
-Right now the shelf is the following:
-  |5|6|7|8|
-  ---------
- 0|1|2|3|4|
-
- State 0 is the beginning
-*/
-
 void loop(){
-  while (Serial.available() == 0);
-  int inp = Serial.parseInt();
+    while (Serial.available() == 0);
+    int inp = Serial.parseInt();
 
-  if(inp == 0){
-      goOrigin();
-  }
-  else{
+    if(inp == 0){
+        goOrigin();
+    }
+    else{
 
         //Decide how much it needs to move horizontally
-        int hrz = hrzMove(shelf[state][0], shelf[inp][0]);
+        int hrz = hrzMove(cords(state)[0], cords(inp)[0]);
         Serial.print("Horizontal ");
         Serial.println(hrz);
 
         //Decide how much it needs to move vertically
-        int vrt = vrtMove(shelf[state][1], shelf[inp][1]);
+        int vrt = vrtMove(cords(state)[1], cords(inp)[1]);
         Serial.print("Vertical ");
         Serial.println(vrt);
 
@@ -51,17 +40,17 @@ void loop(){
             delay(1000);
         }
         else if(hrz>0){
-          goRight(hrz*800);
-          delay(1000);
+            goRight(hrz*800);
+            delay(1000);
         }
         //Move vertically if necessary
         if(vrt<0){
-          goDown();
-          delay(1100);
+            goDown();
+            delay(1100);
         }
         else if(vrt>0){
-          goUp();
-          delay(1100);
+            goUp();
+            delay(1100);
         }
         //Change the state to the one it just got to
         state = inp;
@@ -70,44 +59,44 @@ void loop(){
 
 //Return how much it needs to move horizontally. Negative is to the left, positive to the right
 int hrzMove(int from, int to){
-  return (to-from);
+    return (to-from);
 }
 
 //Return how much ot needs to move vertically. Negative down, positive up.
 int vrtMove(int from, int to){
-  return (to-from);
+    return (to-from);
 }
 
 //Make the robot go to the right
 void goRight(int i){
-  Serial.println("MOVING RIGHT");
-  motorForward(0, 500);
-  delay(i);
-  motorAllStop();
+    Serial.println("MOVING RIGHT");
+    motorForward(0, 500);
+    delay(i);
+    motorAllStop();
 }
 
 //Make the robot go to the left
 void goLeft(int i){
-  Serial.println("MOVING LEFT");
-  motorBackward(0, 500);
-  delay(i);
-  motorAllStop();
+    Serial.println("MOVING LEFT");
+    motorBackward(0, 500);
+    delay(i);
+    motorAllStop();
 }
 
 //Make the platform go up
 void goUp(){
-  Serial.println("MOVING UP");
-  motorBackward(1, 200);
-  delay(5000);
-  motorStop(1);
+    Serial.println("MOVING UP");
+    motorBackward(1, 200);
+    delay(5000);
+    motorStop(1);
 }
 
 //Make the platform go down
 void goDown(){
-  Serial.println("MOVING DOWN");
-  motorForward(1, 200);
-  delay(5000);
-  motorStop(1);
+    Serial.println("MOVING DOWN");
+    motorForward(1, 200);
+    delay(5000);
+    motorStop(1);
 }
 
 //Go to the origin
@@ -119,7 +108,7 @@ void goOrigin(){
     Serial.println("Arrived to origin");
     motorStop(0);
     //If the platform is up, move it down
-    if(shelf[state][1] != 0){
+    if(cords(state)[1] != 0){
         goDown();
     }
     state = 0;
