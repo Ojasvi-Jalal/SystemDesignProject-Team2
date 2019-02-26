1dfebc9d6f8fff8f0500bbb345c829b92e5c6bac
Sinan Vanlı <sinan.vanli@gmail.com>
2019-02-26 14:29:33 +0000

    Distance based motion

--- Arduino/Experimental/SDPMotorStepper/SDPMotorStepper.ino
+++ Arduino/Experimental/SDPMotorStepper/SDPMotorStepper.ino
@@ -6,6 +6,9 @@ using namespace std;
 
 #define ROTARY_SLAVE_ADDRESS 5
 #define ROTARY_NUM 6
+#define MIN_SPEED 70
+#define HORIZONTAL_MOTOR 0
+#define VERTICAL_MOTOR 1
 
 // angle is the motor's angle.
 int angles[ROTARY_NUM] = {0};
@@ -33,23 +36,58 @@ void loop(){
     }
 }
 
-void goAngle(int delta, int motor){
-    motorForward(motor, 100);
-    do{
-        // Request motor deltas
-        Wire.requestFrom(ROTARY_SLAVE_ADDRESS, ROTARY_NUM);
-        if(Wire.available()){
-            int incoming[ROTARY_NUM] = {0};
-            for(int x = 0; x < ROTARY_NUM; x++){
-                incoming[x] = (int8_t) Wire.read();
-            }
-            angles[motor] = angles[motor] + incoming[motor];
-        }
-        Serial.println((String) angles[motor]);
-        delay(20);
+int goAngle(int delta, int motor){
+    if(motor == HORIZONTAL_MOTOR){
+        if(delta > 0)   return moveF(HORIZONTAL_MOTOR, delta);
+        else            return moveB(VERTICAL_MOTOR, -delta);
+    }if(motor == VERTICAL_MOTOR){
+        if(delta > 0)   return moveB(HORIZONTAL_MOTOR, delta);
+        else            return moveF(VERTICAL_MOTOR, -delta);
+    }
+}
 
-    }while(angles[motor] < delta);
+int moveF(int motor, int d){
+    int s = 0;
+    if(d < 0){
+        Serial.printf("Trying to move negative forwards, don't do this!");
+        return -1;
+    }
+    do{
+        s += read(motor);
+        if((d-s) < MIN_SPEED){
+            motorForward(motor, MIN_SPEED)
+        }else if((d-s) < 100){
+            motorForward(motor, (d-s))
+        }else{
+            motorForward(motor, 100);
+        }
+    }while(d > s);
     motorAllStop();
+    delay(100);
+    s += read(motor);
+    return s;
+
+}
+int moveB(int motor, int d){
+    int s = 0;
+    if(d < 0){
+        Serial.printf("Trying to move negative backwards, don't do this!");
+        return -1;
+    }
+    do{
+        s += read(motor);
+        if((d+s) < MIN_SPEED){
+            motorBackward(motor, MIN_SPEED)
+        }else if((d+s) < 100){
+            motorBackward(motor, (d+s))
+        }else{
+            motorBackward(motor, 100);
+        }
+    }while(d > -s);
+    motorAllStop();
+    delay(100);
+    s += read(motor);
+    return s;
 }
 
 void goOrigin(){
@@ -66,4 +104,19 @@ void goOrigin(){
       for(int x = 0; x < ROTARY_NUM; x++){
         incoming[x] = (int8_t) Wire.read();
       }
+}
+
+int read(int motor){
+    Wire.requestFrom(ROTARY_SLAVE_ADDRESS, ROTARY_NUM);
+    if(Wire.available()){
+        int incoming[ROTARY_NUM] = {0};
+        for(int x = 0; x < ROTARY_NUM; x++){
+            incoming[x] = (int8_t) Wire.read();
+        }
+        return incoming[motor];
+    }
+    else{
+        Serial.printf("Couldn't read motor sensors");
+        return 0;
+    }
 }
